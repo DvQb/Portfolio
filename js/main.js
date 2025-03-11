a7ecc09f047e54e95eba7f24425702d63e343bb2
DvQb <davidresendiz753@gmail.com>
2025-03-11 17:55:14 -0600

    se agrega slider y scrollbar para la seccion de proyectos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const toggleButton = document.getElementById("toggle-button");
 const body = document.body;
 
@@ -5,8 +6,6 @@ const body = document.body;
 if (localStorage.getItem("light-mode") === "enabled"){
     body.classList.add("light-mode");
 }
-
-
 
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('light-mode');
@@ -16,4 +15,28 @@ if (body.classList.contains("light-mode")){
     else {localStorage.setItem("light-mode", "disabled")}
 
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 
